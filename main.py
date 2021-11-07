e4e37d627e276afea81820c75c1497de7b5ee628
PootisMaan <dborodin836@gmail.com>
2021-11-07 13:12:46 +0200

    5

--- main.py
+++ main.py
@@ -72,19 +72,18 @@ while (str.upper() == "ОБЫЧНЫЙ" or str == '1'):
 
 
 
-
-
 while (str.upper() == "ИНЖЕНЕРНЫЙ" or str == "2"):
 
     calc_engi = AdvancedCalc()
 
-    calc_engi.set_num1(15)
-    calc_engi.set_num2(12)
+    calc_engi.set_num1(float(input("Введите первое число :")))
+    calc_engi.set_num2(float(input("Введите второе число :")))
+
 
     print("Выберите операцию : ")
+    print("1.Add  2.Minus 3.Multi \n4.Devided 5.Change first 6.Change second \n 8.Transfer to KBytes 7.Exit")
+    operation = input()
 
-print("Вы вышли из калькулятора.")
+    print(dicta[operation]())
 
-print("Вы запустили супер калькулятор")
 
-str = input("Выберите калькулятор 'Обычный'(1) или 'Инженерный'(2): ")
