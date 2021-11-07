914f0aebcbd79e2cb008140fb5a34ba3f9479f96
Daleever <daleever1994@gmail.com>
2021-11-07 12:37:42 +0300

    Red

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-class calc:
+from AdvancedCalc import *
+
+print("Вы запустили супер калькулятор")
+
+str = input("Выберите калькулятор 'Обычный'(1) или 'Инженерный'(2): ")
+
+while(str.upper()=="ОБЫЧНЫЙ" or str == '1'):
+    calc = OrdinaryCalc()
+    
+    ClearCounter = 0
+    
+    calc.set_num1 (float(input("Введите первое число: ")))
+    calc.set_num2 (float(input("Введите второе число: ")))
+    
+    dicta = {"1":calc.Add,"2":calc.Sub}
+        
+    print("Выберите операцию")
+    print("1.Add  2.Minus 3.Multi \n4.Devided 5.Change first 6.Change second \n7.Exit")
+    
+    if(operation == 1):
+        print(calc.Add())
+    print(dicta[operation]())
+    
+    
 
