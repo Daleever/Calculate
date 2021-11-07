1a2e41a9f7e39fdc7aca616d96567f9ba6ee8131
PootisMaan <dborodin836@gmail.com>
2021-11-07 11:49:57 +0200

    OrdinaryCalc

---
+++ OrdinaryCalc.py
@@ -0,0 +1,32 @@
+class OrdinaryCalc:
+
+    def set_num1(self, num1):
+        self.num1 = num1
+
+
+    def get_num1(self):
+        return self.num1
+
+
+    def get_num2(self):
+        return self.num2
+
+
+    def set_num2(self, num2):
+         self.num2 = num2
+
+
+    def Add(self):
+        return self.num1 + self.num2
+
+
+    def Div(self):
+        return self.num1 / self.num2
+
+
+    def Mul(self):
+        return self.num1 * self.num2
+
+
+    def Sub(self):
+        return self.num1 - self.num2
